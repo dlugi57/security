08a2ede4f62e35794467805cc1f19bfb0ee5af7d
dlugi57 <dlugoszpeter@gmail.com>
2020-11-26 00:13:41 +0100

    INITIALIZE

---
+++ src/main/java/com/openclassrooms/security/configuration/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.openclassrooms.security.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests()
+                   // .antMatchers("/user/**").hasAnyAuthority("ADMIN")
+                   // .antMatchers("/*").hasAnyAuthority("ADMIN", "USER")
+                    .antMatchers("/admin").hasRole("ADMIN")
+                    .antMatchers("/user").hasRole("USER")
+                    .anyRequest().authenticated()
+                    .and()
+                    .formLogin();
+
+
+        // super.configure(web);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+       // BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+        auth.inMemoryAuthentication()
+                .withUser("springuser").password(passwordEncoder().encode("spring123")).roles("USER")
+                .and()
+                .withUser("springadmin").password(passwordEncoder().encode("admin123")).roles("ADMIN", "USER");
+    }
+
+/*    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }*/
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
+
+
---
+++ src/main/java/com/openclassrooms/security/controller/LoginController.java
@@ -0,0 +1,23 @@
+package com.openclassrooms.security.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.security.RolesAllowed;
+
+@RestController
+public class LoginController {
+    @RolesAllowed("USER")
+    @RequestMapping("/*")
+    public String getUser()
+    {
+        return "Welcome User";
+    }
+
+    @RolesAllowed({"USER","ADMIN"})
+    @RequestMapping("/admin")
+    public String getAdmin()
+    {
+        return "Welcome Admin";
+    }
+}
